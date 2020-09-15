610daf89ec7d8aa9932c7e8211a3483ffab02743
jyothsna <jyothsnak89@gmail.com>
2020-09-15 20:36:35 +0530

    changes

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/spacex-angular'));
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
-// });
-app.use(express.static(__dirname+'/dist'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
