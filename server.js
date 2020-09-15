3d5afd3f52b5df9129accc3031cd0eaa816b5e25
jyothsnaknr <42332538+jyothsnaknr@users.noreply.github.com>
2020-09-15 14:16:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spacex-angular'));
+// app.use(express.static(__dirname + '/dist/spacex-angular'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
+// });
+app.use(express.static(__dirname+'/spacex-angular/dist'));
+app.get('*', (req, res) => {
+    res.sendFile('./spacex-angular/dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
