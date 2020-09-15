5b3826182e810805c43ce6007d6041afcb2b21a9
jyothsna <jyothsnak89@gmail.com>
2020-09-15 19:51:25 +0530

    heroku changes

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spacex-angular'));
+// app.use(express.static(__dirname + '/dist/spacex-angular'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
+// });
+app.use(express.static(__dirname+'/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,16 +1,44 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppService', () => {
-  let service: AppService;
+    let service: AppService;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule], 
+          providers: [AppService]
+      });
+      service = TestBed.inject(AppService);
+    });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
 });
+
+  // describe('AppService', () => {
+  //   let service: AppService;
+  //   beforeEach(() => { service = new AppService(); });
+  
+  //   it('#getValue should return real value', () => {
+  //     expect(service.getValue()).toBe('real value');
+  //   });
+  
+  //   it('#getObservableValue should return value from observable',
+  //     (done: DoneFn) => {
+  //     service.getObservableValue().subscribe(value => {
+  //       expect(value).toBe('observable value');
+  //       done();
+  //     });
+  //   });
+  
+  //   it('#getPromiseValue should return value from a promise',
+  //     (done: DoneFn) => {
+  //     service.getPromiseValue().then(value => {
+  //       expect(value).toBe('promise value');
+  //       done();
+  //     });
+  //   });
+  // });
