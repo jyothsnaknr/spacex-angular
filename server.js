a3e908ac9532cc95ff0a406bd6eb9e0e41e391d5
jyothsnaknr <42332538+jyothsnaknr@users.noreply.github.com>
2020-09-15 19:43:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,22 @@ const app = express();
     
 // res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html'));
 // });
-app.use(express.static(__dirname+'/dist/spacex-angular'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html')); 
-});
+
+
+// app.use(express.static(__dirname+'/dist/spacex-angular'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/dist/spacex-angular/index.html')); 
+// });
+
+app.use(express.static(__dirname+'/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+//app.listen(process.env.PORT || 8080);
+
+
